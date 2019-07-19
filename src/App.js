f073c1c31f570fd047b775aec9070a89ea36a22d
Teatoller <steven.otieno1@gmail.com>
2019-07-19 13:51:30 +0300

    (component):implement store in component
    - import store, add to component
    
    [Starts #007]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Counter from './Counter';
+import store from './store';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Counter count="7" />
+      <Counter store={store} />
     </div>
   );
 }
